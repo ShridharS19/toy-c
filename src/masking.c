33ac4619c5be243cc755a657dff3630122e39de5
Shridhar Sinha <shridharsinha4@gmail.com>
2025-09-13 12:50:08 +0000

    computer programs exercise 2.7

---
+++ src/masking.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define MASK 0xFF
+typedef char *byte_pointer;
+
+void show_bytes(byte_pointer start, int len)
+{
+    int i;
+    for (i = 0; i < len; i++)
+    {
+        printf(" %02x", (unsigned char) start[i]);
+    }
+    printf("\n");
+}
+int mask1(int var){
+    return(var|~MASK);
+}
+int mask2(int var){
+    return(var^MASK);
+}
+int mask3(int var){
+    return(var&~MASK);
+}
+int main(void){
+    int test = 0x98FDECBA;
+    int test1 = mask1(test);
+    show_bytes((byte_pointer) &test1, sizeof(test));
+    int test2 = mask2(test);
+    show_bytes((byte_pointer) &test2, sizeof(test));
+    int test3 = mask3(test);
+    show_bytes((byte_pointer) &test3, sizeof(test));
+    return 0;
+}
